0f7d699a18c7bf7e43be9c40d65646a1ae0d643e
Viktoriia <mwa10@ukr.net>
2025-04-12 20:00:25 +0300

    HW-2-mongodb

---
+++ src/server.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { getAllContacts, getContactById } from './services/contacts.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:id', async (req, res) => {
+        const { id } = req.params;
+        const contact = await getContactById(id);
+
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found',
+            });
+            return;
+        }
+
+        res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${id}!`,
+            data: contact,
+        });
+    });
+
+    app.use((req, res) => {
+        res.status(404).json({
+            message: 'Not found',
+        });
+    });
+
+    app.use((err, req, res, next) => {
+        res.status(500).json({
+            message: 'Something went wrong',
+            error: err.message,
+        });
+        next();
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
